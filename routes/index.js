acb4699ab640278cc938c8bacc57f83d212b7e18
Anjany Pandey <anjany.pandey06@gmail.com>
2024-12-01 18:27:42 +0530

    "Updated routes and header template: added isLoggedIn middleware, product model, and logout route; modified header to display navigation links based on login status"

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const isLoggedin = require("../middlewares/isLoggedIn")
+const isLoggedin = require("../middlewares/isLoggedIn");
+const productModel = require("../models/product-model");
 
 router.get("/", function(req,res){
   let error = req.flash("error");
-  res.render("index",{error});
+  res.render("index",{error, loggedin : false});
 });
 
-router.get("/shop", isLoggedin, function(req, res){
-  res.render("shop");
+router.get("/shop", isLoggedin, async function(req, res){
+ let products = await productModel.find()
+  res.render("shop", {products});
 })
+router.get("/logout",isLoggedin, function(req,res){
+  res.render("shop");
+});
+
 
 module.exports = router;
